d7b39acb18392d7f25dd781c4c288e6ad22a7089
Liliya Artyukh <lartyukh@cencol.ca>
2019-01-19 14:14:11 -0500

    class 2 - variables deck

---
+++ class2/src/variables/Practice_1.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class Practice_1 {
+
+	public static void main(String[] args) {
+		String firstName;
+		firstName = "Bob";
+		int age = 25;
+		char grade = 'A';
+		boolean isStudent = true;
+		double price = 200.00;
+
+		Human human = new Human();
+
+		System.out.println("name: " + firstName );
+		System.out.println("age: " + age );
+		System.out.println("grade: " + grade );
+		System.out.println("isStudent: " + isStudent );
+		System.out.println("price: " + price );
+		System.out.println("human: " + human );
+
+	}
+
+}
