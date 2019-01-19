2081dbf24b59ac38c7b7ade4c3e9b3d346eccec3
Liliya Artyukh <lartyukh@cencol.ca>
2019-01-19 16:04:28 -0500

    read user input

---
+++ class2/src/read_input/Driver.java
@@ -0,0 +1,32 @@
+package read_input;
+
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		String name;
+		int age;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please provide name: ");
+		
+		name = scanner.nextLine();
+		
+		System.out.println("Please provide your age: ");
+		
+		//age = scanner.nextInt();
+		
+		//age = Integer.parseInt(scanner.nextLine());
+		
+		
+		String ageString = scanner.nextLine();
+		age = Integer.parseInt(ageString);
+		
+		System.out.printf("Your name is: %s. You  are %d years old.", name, age);
+				
+	}
+
+}
