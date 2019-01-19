c84d90230fd9f7668e0f1103149689e74fbd8a3c
liliya0artyukh <liliya0artyukh@gmail.com>
2019-01-18 21:19:40 -0500

    class 1 work

---
+++ lab1nameclass1/src/first_program/Driver.java
@@ -0,0 +1,21 @@
+package first_program;
+
+/** 
+ * 
+ * @author lartyukh
+ *
+ */
+public class Driver {
+	public static void main(String[] args) {
+
+		System.out.println("Hello Students!");//it is more logical to say hello students
+		/*it is more logical
+		to say hello students
+		  
+		 */
+		
+		String name = "Liliya";
+		
+		
+	}
+}
---
+++ lab1nameclass1/src/first_program/MyTest.java
@@ -0,0 +1,14 @@
+package first_program;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
